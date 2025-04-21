2fa32b1216263ff8526028631a96bf00dd5e6d19
RocketDC <divyanand101998.studystuff@gmail.com>
2025-04-21 12:50:58 +0530

    Added new data fetching methods

--- src/test/java/api_automation/SmsApi.java
+++ src/test/java/api_automation/SmsApi.java
@@ -29,21 +29,92 @@ String moduleName = "sms";
         setExtentTest(test);
         logger.info("Verifying the SMS API");
         // Test method to send an SMS
-        Response response = sendSms();
+        Response response = sendSms(getProperty("mobileNumber"), "Hello, this is a test message.", getProperty("api_key"));
         if (response.getStatusCode() == 200) {
             System.out.println("SMS sent successfully.");
         } else {
             System.out.println("Failed to send SMS. Status code: " + response.getStatusCode());
         }
+
+
+    }
+
+    @Test(priority = 2)
+    public void testSendSms_without_APiKey() {
+        ExtentTest test = extent.createTest("TC" + getClass() + "testSendSms_without_APiKey", "Verifying if user can't send a message without api key").assignCategory(moduleName );
+        setExtentTest(test);
+        logger.info("Verifying if user can't send a message without api key");
+        // Test method to send an SMS
+        Response response = smsRequest(getProperty("mobileNumber"), "Hello, this is a test message.", "" )
+                .post(getProperty("sms_url")+ getProperty("sms_path")).then().extract().response();
+        if( response.getStatusCode() == 200) {
+
+            test.log(Status.FAIL,"SMS sending shouldn't be allowed without api key" + response.getStatusCode() + "\nResponse Body: " + response.getBody().asString());
+            logger.info("SMS sending shouldn't be allowed without api key" + response.getStatusCode() + "\nResponse Body: " + response.getBody().asString());
+        } else {
+            logApiResponse(response.getBody().asString());
+            test.log(Status.PASS,"SMS sent Failed without api key. Status code: " + response.getStatusCode());
+            logger.info("SMS sent Failed without api key. Status code: " + response.getStatusCode());
+        }
+        System.out.println("Response: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+
+
+    }
+
+    @Test(priority = 3)
+    public void testSendSms_without_message() {
+        ExtentTest test = extent.createTest("TC" + getClass() + "testSendSms_without_message", "Verifying if user can't send sms without message").assignCategory(moduleName );
+        setExtentTest(test);
+        logger.info("Verifying if user can't send sms without message");
+        // Test to send an SMS without message
+        Response response = smsRequest(getProperty("mobileNumber"), "", getProperty("api_key") )
+                .post(getProperty("sms_url")+ getProperty("sms_path")).then().extract().response();
+        if( response.getStatusCode() == 200) {
+
+            test.log(Status.FAIL,"SMS sending shouldn't be allowed without message" + response.getStatusCode() + "\nResponse Body: " + response.getBody().asString());
+            logger.info("SMS sending shouldn't be allowed without message" + response.getStatusCode() + "\nResponse Body: " + response.getBody().asString());
+        } else {
+            logApiResponse(response.getBody().asString());
+            test.log(Status.PASS,"SMS sent Failed without message. Status code: " + response.getStatusCode());
+            logger.info("SMS sent Failed without message. Status code: " + response.getStatusCode());
+        }
+        System.out.println("Response: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+
+
+    }
+
+    @Test(priority = 4)
+    public void testSendSms_without_ValidPhoneNumber() {
+        ExtentTest test = extent.createTest("TC" + getClass() + "testSendSms_without_ValidPhoneNumber", "Verifying if user can't send sms without valid phone number").assignCategory(moduleName );
+        setExtentTest(test);
+        logger.info("Verifying if user can't send sms without valid phone number");
+        // Test to send an SMS without message
+        Response response = smsRequest("alpha", "Hello, this is a test message.", getProperty("api_key") )
+                .post(getProperty("sms_url")+ getProperty("sms_path")).then().extract().response();
+        if( response.getStatusCode() == 200) {
+
+            test.log(Status.FAIL,"SMS sending shouldn't be allowed without valid phone number" + response.getStatusCode() + "\nResponse Body: " + response.getBody().asString());
+            logger.info("SMS sending shouldn't be allowed without valid phone number" + response.getStatusCode() + "\nResponse Body: " + response.getBody().asString());
+        } else {
+            logApiResponse(response.getBody().asString());
+            test.log(Status.PASS,"SMS sent Failed without valid phone number. Status code: " + response.getStatusCode());
+            logger.info("SMS sent Failed without valid phone number. Status code: " + response.getStatusCode());
+        }
+        System.out.println("Response: " + response.getStatusCode());
+        System.out.println("Response Body: " + response.getBody().asString());
+
+
     }
 
 
     // Method to send an SMS
-    public RequestSpecification smsRequest(String phoneNumber, String message) {
+    public RequestSpecification smsRequest(String phoneNumber, String message, String apiKey) {
 
         RequestSpecification requestSpecification = given()
-                .formParam("api_key", System.getProperty("api_key"))
-                .formParam("api_secret", System.getProperty("api_secret"))
+                .formParam("api_key", apiKey)
+                .formParam("api_secret", getProperty("api_secret"))
                 .formParam("to", phoneNumber)
                 .formParam("from", "Nexmo")
                 .formParam("text", message);
@@ -51,11 +122,11 @@ String moduleName = "sms";
         System.out.println("Sending SMS to " + phoneNumber + ": " + message);
         return requestSpecification;
     }
-public Response sendSms(){
+public Response sendSms(String phoneNumber, String message, String apiKey){
     ExtentTest test = extent.createTest("TC" + getClass() + "sendSms", "Sending the sms request").assignCategory(moduleName );
     setExtentTest(test);
     // Code to send an SMS using the API
-    Response response = smsRequest(getProperty("mobileNumber"), "Hello, this is a test message.")
+    Response response = smsRequest(phoneNumber, message,apiKey )
             .post(getProperty("sms_url")+ getProperty("sms_path")).then().extract().response();
     if( response.getStatusCode() != 200) {
         handleApiError("Failed to send SMS. Status code: " + response.getStatusCode() + "\nError: " + response.getBody().asString());
